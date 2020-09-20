71293bcf149bf4f280b2e3b6be8115d6e385b2d1
Soyoun Kim <makeajourney@gmail.com>
2020-09-21 03:50:29 +0900

    Added CRU APIs of Posts

---
+++ src/main/java/com/makeajourney/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.makeajourney.book.springboot.web.dto;
+
+import com.makeajourney.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
