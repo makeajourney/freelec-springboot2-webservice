1fe8ef34c553587c52b04cdcc2db67c2bd33e55b
Soyoun Kim <makeajourney@gmail.com>
2020-10-04 21:52:24 +0900

    Added delete view and api

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -9,6 +9,10 @@ var index = {
         $('#btn-update').on('click', function () {
             _this.update();
         })
+
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        })
     },
     save : function () {
         var data = {
@@ -51,6 +55,21 @@ var index = {
             alert(JSON.stringify(error));
         });
     },
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function () {
+            alert('글이 삭제되었습니다.')
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
 };
 
 index.init();
