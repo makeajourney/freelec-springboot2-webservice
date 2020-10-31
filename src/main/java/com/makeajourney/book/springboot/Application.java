6152e03914bc47b428de81101b50f84b2dd91b4f
Soyoun Kim <makeajourney@gmail.com>
2020-10-31 13:00:36 +0900

    기존 테스트에 시큐리티 적용

--- src/main/java/com/makeajourney/book/springboot/Application.java
+++ src/main/java/com/makeajourney/book/springboot/Application.java
@@ -3,9 +3,7 @@ package com.makeajourney.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
---
+++ src/main/java/com/makeajourney/book/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.makeajourney.book.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
